4351ddb9a1c5a5da110f0a543dae90adfb356a8e
aki90 <jjshh9095@naver.com>
2023-05-25 22:11:41 +0900

    fea: SearchHeader, UI 구현

--- src/components/SearchHeader/SearchHeader.tsx
+++ src/components/SearchHeader/SearchHeader.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styles from './SearchHeader.module.scss';
+import { Link } from 'react-router-dom';
+import { BsSearch, BsYoutube } from 'react-icons/bs';
 
 export default function SearchHeader() {
-  return <div>SearchHeader</div>;
+  return (
+    <header className={styles.header}>
+      <Link to="/" className={styles.header__title}>
+        <BsYoutube className={styles.logo} />
+        <h1>YouTube</h1>
+      </Link>
+
+      <form className={styles.header__search}>
+        <input type="text" placeholder="검색" />
+        <button type="submit">
+          <BsSearch className={styles.icon} />
+        </button>
+      </form>
+    </header>
+  );
 }
