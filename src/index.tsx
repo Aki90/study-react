c22e1c5aaee73755cb82fff62988aa90bf566bca
aki90 <jjshh9095@naver.com>
2023-05-25 22:47:17 +0900

    fea: route 정리 및 사용하지 않는 App 컴포넌트 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.scss';
-import App from './App';
 import NotFound from './pages/NotFound';
 import Videos from './pages/Videos';
 import VideoDetail from './pages/VideoDetail';
+import SearchHeader from '@/components/SearchHeader/SearchHeader';
+import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
@@ -14,10 +14,10 @@ const root = createRoot(container as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <SearchHeader />
+
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Videos />} />
-          <Route path="videos" element={<Videos />} />
+        <Route path="/" element={<Videos />}>
           <Route path="videos/:keyword" element={<Videos />} />
           <Route path="videos/watch/:videoId" element={<VideoDetail />} />
         </Route>
