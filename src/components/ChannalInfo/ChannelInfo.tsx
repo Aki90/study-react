b39835529e53841639a9fddaf00d2ff7b02094d5
aki90 <jjshh9095@naver.com>
2023-05-27 20:45:46 +0900

    fix: 배포 위한 lint 주석처리

--- src/components/ChannalInfo/ChannelInfo.tsx
+++ src/components/ChannalInfo/ChannelInfo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { fetchChannelImageURL } from '@/API/channels';
 import { useQuery } from '@tanstack/react-query';
 import Loading from '../common/Loading';
