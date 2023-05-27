4e16c0453bde919473f097927542551e005b3459
aki90 <jjshh9095@naver.com>
2023-05-27 19:44:45 +0900

    fea: ChannelInfo 컴포넌트 구현

--- src/components/ChannalInfo/ChannelInfo.tsx
+++ src/components/ChannalInfo/ChannelInfo.tsx
@@ -1,3 +1,8 @@
+import { fetchChannelImageURL } from '@/API/channels';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../common/Loading';
+import Error from '../common/Error';
+
 export default function ChannalInfo({
   id,
   name,
@@ -5,5 +10,31 @@ export default function ChannalInfo({
   id: string;
   name: string;
 }) {
-  return <div style={{ border: '1px solid red' }}>{name}</div>;
+  async function fakeUrl() {
+    const data =
+      'https://yt3.ggpht.com/8IG_vczSZLUYnDvfHFusOMdIFRpPP8xoKX6z2BLoyALI2hep-PrlLjKEp8qnrnOjTDbAF2w4kQ=s88-c-k-c0x00ffffff-no-nd-rj';
+
+    return data;
+  }
+
+  const {
+    isLoading,
+    isError,
+    data: url,
+  } = useQuery({
+    queryKey: ['channel', id],
+    // queryFn: () => fetchChannelImageURL(id),
+    queryFn: () => fakeUrl(),
+    staleTime: 1000 * 60 * 1,
+    retry: 3,
+  });
+
+  return (
+    <div>
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      {url && <img src={url} alt={name} />}
+      <p></p>
+    </div>
+  );
 }
